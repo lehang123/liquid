04483f4170cdda40068892b067f7d6c0c28689d9
lehang123 <gonglehan2014@gmail.com>
2019-08-07 01:05:03 +1000

    Added main navigation controller to connect main and login View

---
+++ ITProject/ITProject/MainNavigationController.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationController.swift
+//  ITProject
+//
+//  Created by Gong Lehan on 7/8/19.
+//  Copyright © 2019 liquid. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainNavigationController :UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
