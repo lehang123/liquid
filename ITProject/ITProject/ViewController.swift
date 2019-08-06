e0d02a69baab2de892c931f3024eeb2e230a071d
lehang123 <gonglehan2014@gmail.com>
2019-08-06 15:11:33 +1000

    init project

---
+++ ITProject/ITProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ITProject
+//
+//  Created by Gong Lehan on 6/8/19.
+//  Copyright © 2019 liquid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
