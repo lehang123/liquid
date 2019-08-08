1c696d04d240095b965a4bd14b62951e9b4b690a
chenghongz <chenghongz@unimelb.edu.au>
2019-08-08 23:09:09 +1000

    Add login button.  Still need to handle "error" to give user messages.  Also spinner is moved.

--- ITProject/ITProject/LoginViewController.swift
+++ ITProject/ITProject/LoginViewController.swift
@@ -8,14 +8,33 @@
 
 import Foundation
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var ID: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func LoginButton(_ sender: Any) {
+        let account: String = ID.text!
+        let pw: String = password.text!
+        Auth.auth().signIn(withEmail: account, password: pw) { [weak self] user, error in
+            guard let strongSelf = self
+                else { return }
+            // ...
+            print ("asdasigjiosdjgiosdjgiodsiagasiohgiosdagiosdhioghsioghweiogjiowejgiowejgiowejgioewjiogjweiogjewiojgiowejgiowejgoiwejgiowejgioewjgiojweiogjweiogjewiojgeiowjgioewjg")
+            print (strongSelf)
+            print (user)
+            print (error)
+        }
+        
+        
+    }
+    
 }
 
