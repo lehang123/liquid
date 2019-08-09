d1b9802b5cb9b1e988a3628464a482daa3de7470
chenghongz <chenghongz@unimelb.edu.au>
2019-08-09 15:47:06 +1000

    Add more information about account.

--- ITProject/ITProject/Util.swift
+++ ITProject/ITProject/Util.swift
@@ -13,6 +13,8 @@ class Util {
     
     public static var ACCOUNT_INCORRECT_TITLE = "Email/Password Incorrect"
     public static var ACCOUNT_INCORRECT_MESSAGE = "Try Again"
+    public static var CONFIRMED_INCORRECT_WRONG = "Wrong Password"
+    public static var PASSWORD_LENGTH_NOT_ENOUTH = "Minimum length is 8"
     public static var BUTTON_DISMISS = "dismiss"
     public static var CREATE_INCORRECT = "Invalid Email Address"
     public static var CREATE_CORRECT = "Congratulation!"
