cf253a2283e042400fd97395d62216fc60bd1a8b
chenghongz <chenghongz@unimelb.edu.au>
2019-08-09 15:17:47 +1000

    Clean up the email login class

--- ITProject/ITProject/EmailLoginViewController.swift
+++ ITProject/ITProject/EmailLoginViewController.swift
@@ -22,39 +22,45 @@ class EmailLoginViewController : UIViewController {
     @IBAction func CreateButtonOnTouch(_ sender: Any) {
                 let email: String = emailAddress.text!
                 let pw: String = password.text!
-                Auth.auth().createUser(withEmail: email, password: pw) {
-                    authResult, error in
-                    
-                    if error != nil {
-                        // Show error message to users to let them use correct email form
-                        let alertController = UIAlertController(title: Util.CREATE_INCORRECT, message:
-                            Util.ACCOUNT_INCORRECT_MESSAGE, preferredStyle: .alert)
-                        alertController.addAction(UIAlertAction(title: Util.BUTTON_DISMISS, style: .default))
-                        self.present(alertController, animated: true, completion: nil)
-                    } else {
-
-                        // Show the users that the account has been sucessfully created
-                        let alertController = UIAlertController(title: Util.CREATE_CORRECT, message:
-                            Util.CREATE_CORRECT_MESSAGE, preferredStyle: .alert)
-                        
-                        // Back to the previous view controller for user to log in
-                        
-                        let changeView = UIAlertAction(title: "BACK", style: .default){action in
-                            // I know I need to put something in here.
-                            let next = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController")
-                            self.present(next!, animated:true, completion:nil)
-                        }
-                        //self.present(next!, animated: true, completion: nil)
-                        alertController.addAction(UIAlertAction(title: Util.BUTTON_DISMISS, style: .default))
-                        alertController.addAction(changeView)
-                        
-                        
-                        self.present(alertController, animated: true, completion: nil)
-                        
-                        
-                    }
-                }
+                authentiate(email: email, pw: pw)
     }
+    
+    // Authentiate process is here
+    func authentiate(email: String, pw: String) {
+        Auth.auth().createUser(withEmail: email, password: pw) {
+            authResult, error in
+            
+            if error != nil {
+                // Show error message to users to let them use correct email form
+                self.alert(first_p: Util.CREATE_INCORRECT,second_p:Util.ACCOUNT_INCORRECT_MESSAGE,third_p: Util.BUTTON_DISMISS)
+            } else {
+                // Show the users that the account has been sucessfully created
+                let alertController = UIAlertController(title: Util.CREATE_CORRECT, message:
+                    Util.CREATE_CORRECT_MESSAGE, preferredStyle: .alert)
+                
+                // Back to the previous view controller for user to log in
+                let changeView = UIAlertAction(title: "BACK", style: .default){action in
+                    let next = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController")
+                    self.present(next!, animated:true, completion:nil)
+                }
+                
+                // Add all actions to alert controller
+                alertController.addAction(UIAlertAction(title: Util.BUTTON_DISMISS, style: .default))
+                alertController.addAction(changeView)
+                self.present(alertController, animated: true, completion: nil)
+                
+            }
+        }
+    }
+    
+    // Show alert message to users
+    func alert(first_p: String, second_p: String, third_p: String) {
+        let alertController = UIAlertController(title: first_p, message:second_p, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: third_p, style: .default))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    
     
     
     
