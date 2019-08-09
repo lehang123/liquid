a1cb2164918f2733e07ce3eaafabccc0ef0b7cfd
chenghongz <chenghongz@unimelb.edu.au>
2019-08-09 14:26:49 +1000

    Add new feautres on Email Login View Controller.  Add some messages to Users

--- ITProject/ITProject/EmailLoginViewController.swift
+++ ITProject/ITProject/EmailLoginViewController.swift
@@ -26,6 +26,31 @@ class EmailLoginViewController : UIViewController {
                     authResult, error in
                     
                     if error != nil {
+                        // Show error message to users to let them use correct email form
+                        let alertController = UIAlertController(title: Util.CREATE_INCORRECT, message:
+                            Util.ACCOUNT_INCORRECT_MESSAGE, preferredStyle: .alert)
+                        alertController.addAction(UIAlertAction(title: Util.BUTTON_DISMISS, style: .default))
+                        self.present(alertController, animated: true, completion: nil)
+                    } else {
+
+                        // Show the users that the account has been sucessfully created
+                        let alertController = UIAlertController(title: Util.CREATE_CORRECT, message:
+                            Util.CREATE_CORRECT_MESSAGE, preferredStyle: .alert)
+                        
+                        // Back to the previous view controller for user to log in
+                        
+                        let changeView = UIAlertAction(title: "BACK", style: .default){action in
+                            // I know I need to put something in here.
+                            let next = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController")
+                            self.present(next!, animated:true, completion:nil)
+                        }
+                        //self.present(next!, animated: true, completion: nil)
+                        alertController.addAction(UIAlertAction(title: Util.BUTTON_DISMISS, style: .default))
+                        alertController.addAction(changeView)
+                        
+                        
+                        self.present(alertController, animated: true, completion: nil)
+                        
                         
                     }
                 }
