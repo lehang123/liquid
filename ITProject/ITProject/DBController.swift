e35441623ac2e5140f161ee09ec0dd7d6dac586a
Gilbert Vincenta <gilbert11323@gmail.com>
2019-08-09 15:49:17 +1000

    updated dependencies, added DBController

---
+++ ITProject/ITProject/DBController.swift
@@ -0,0 +1,16 @@
+//
+//  Util.swift
+//  ITProject
+//
+//  Created by Gilbert Vincenta on 9/8/19.
+//  Copyright © 2019 liquid. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import FirebaseCore
+import FirebaseFirestore
+
+class DBController {
+    
+}
