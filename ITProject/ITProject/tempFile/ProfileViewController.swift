17e4bd9f8c0e1f6c62fa52bd5fd094886f23d18f
Erya Wen <ewenalhyoo@gmail.com>
2019-08-12 10:35:02 +1000

    Added SideMenu, Modified Carousel Effect

---
+++ ITProject/ITProject/tempFile/ProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewController.swift
+//  ITProject
+//
+//  Created by Erya Wen on 2019/8/11.
+//  Copyright © 2019 liquid. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction private func close() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+}
