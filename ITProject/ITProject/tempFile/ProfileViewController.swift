2d5eaf7afc38233ae71e829fd798528bb6ac04a8
chenghongz <chenghongz@unimelb.edu.au>
2019-08-15 22:23:28 +1000

    add profile pictures

--- ITProject/ITProject/tempFile/ProfileViewController.swift
+++ ITProject/ITProject/tempFile/ProfileViewController.swift
@@ -6,19 +6,42 @@
 //  Copyright © 2019 liquid. All rights reserved.
 //
 
+import Firebase
 import UIKit
+import EnhancedCircleImageView
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var profilePicture: EnhancedCircleImageView!
+    @IBOutlet weak var name: UILabel!
+    
+    //@IBOutlet weak var userName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        getName()
+        profilePicture.image=#imageLiteral(resourceName: "tempProfileImage")
+        
+        
     }
     
-
     @IBAction private func close() {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    func getName() {
+        let user = Auth.auth().currentUser
+        if let user = user {
+            // The user's ID, unique to the Firebase project.
+            // Do NOT use this value to authenticate with your backend server,
+            // if you have one. Use getTokenWithCompletion:completion: instead.
+            let uid = user.uid
+            
+            
+            name.text = "test"
+            
+        }
     }
     
 
