6297976934159b2cc93046f7eda8f40651ad8501
Erya Wen <ewenalhyoo@gmail.com>
2019-08-16 16:39:56 +1000

    fixed individual setting ui display bug

---
+++ ITProject/ITProject/Controllers/IndividualSettingTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  IndividualSettingViewContoller.swift
+//  ITProject
+//
+//  Created by 陳信宏保佑🙏 on 2019/8/14.
+//  Copyright © 2019 liquid. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class IndividualSettingTableViewController: UITableViewController {
+    
+    
+//    
+//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 55
+//    }
+    
+
+//
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+    }
+    
+    
+
+    
+
+    
+    
+    
+}
+
