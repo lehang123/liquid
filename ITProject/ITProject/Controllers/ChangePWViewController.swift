c98c026c344158c239b409da472be0912a8586ad
chenghongz <chenghongz@unimelb.edu.au>
2019-08-17 14:43:09 +1000

    add change password function.  "PROBLEM" :: once click login in , cannot present another controller -> for solve

--- ITProject/ITProject/Controllers/ChangePWViewController.swift
+++ ITProject/ITProject/Controllers/ChangePWViewController.swift
@@ -12,12 +12,14 @@ import Firebase
 
 class ChangePasswordViewController : UIViewController {
     
+    private static let PASSWORD_INVALID = "The password is invalid"
     private static let PASSWORD_LENGTH_NOT_ENOUTH = "Minimum length is 8"
     private static let CONFIRMED_INCORRECT_WRONG = "Wrong Password"
     private static let ACCOUNT_INCORRECT_MESSAGE = "Try Again"
     private static let CREATE_CORRECT = "Congratulation!"
     private static let CREATE_CORRECT_MESSAGE = "Account Success!"
 
+    @IBOutlet weak var originalPW: UITextField!
     @IBOutlet weak var newPW: UITextField!
     @IBOutlet weak var confirmedPassword: UITextField!
     
@@ -37,14 +39,23 @@ class ChangePasswordViewController : UIViewController {
         }
 
         if (newPW.text == confirmedPassword.text) {
-            updatePassword(newPW: newPW.text!)
+    
+            let user = Auth.auth().currentUser
+            let credential: AuthCredential = EmailAuthProvider.credential(withEmail: (user?.email)!, password: originalPW.text!)
             
-            let next = self.storyboard?.instantiateViewController(withIdentifier: "FamilyMainPageViewController")
-            self.navigationController?.pushViewController(next!, animated: true)
-            self.present(next!, animated: true, completion: nil)
+            // Prompt the user to re-provide their sign-in credentials
+            user?.reauthenticate(with: credential, completion: {(authResult, error) in
+                if error != nil {
+                    // An error happened.
+                    Util.ShowAlert(title: ChangePasswordViewController.PASSWORD_INVALID, message: ChangePasswordViewController.ACCOUNT_INCORRECT_MESSAGE, action_title: Util.BUTTON_DISMISS, on: self)
+                }else{
+                    // User re-authenticated.
+                    self.updatePassword(newPW : self.newPW.text!)
+                }
+            })
             
-//            Util.ShowAlert(title: ChangePasswordViewController.CREATE_CORRECT, message: ChangePasswordViewController.CREATE_CORRECT_MESSAGE, action_title: ChangePasswordViewController.CREATE_CORRECT, on: next! )
-//
+            
+            
         } else {
             Util.ShowAlert(title: ChangePasswordViewController.CONFIRMED_INCORRECT_WRONG,
                            message: ChangePasswordViewController.ACCOUNT_INCORRECT_MESSAGE,
@@ -57,8 +68,16 @@ class ChangePasswordViewController : UIViewController {
 
     func updatePassword(newPW : String) {
         Auth.auth().currentUser?.updatePassword(to: newPW) { (error) in
-            print ("test")
-            print (error!)
+
+            if (error == nil) {
+                
+                let next = self.storyboard?.instantiateViewController(withIdentifier: "FamilyMainPageViewController")
+                self.navigationController?.pushViewController(next!, animated: true)
+                self.present(next!, animated: true, completion: nil)
+                
+                            Util.ShowAlert(title: ChangePasswordViewController.CREATE_CORRECT, message: ChangePasswordViewController.CREATE_CORRECT_MESSAGE, action_title: ChangePasswordViewController.CREATE_CORRECT, on: next! )
+                
+            }
             
         }
     }
