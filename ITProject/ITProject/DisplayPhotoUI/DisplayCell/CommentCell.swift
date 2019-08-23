0c831f8027ef0968fae62affaa83b31adbe43e0d
lehang123 <gonglehan2014@gmail.com>
2019-08-23 14:39:36 +1000

    creating comment section (expanding view left)

---
+++ ITProject/ITProject/DisplayPhotoUI/DisplayCell/CommentCell.swift
@@ -0,0 +1,35 @@
+//
+//  CommentCell.swift
+//  ITProject
+//
+//  Created by Gong Lehan on 23/8/19.
+//  Copyright © 2019 liquid. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CommentCell: UITableViewCell {
+    
+    private static let COMMENT_ERROR = "Error reading comment"
+    private static let USERNAME_ERROR = "Error reading username"
+    
+    @IBOutlet private weak var commentLabel: UILabel!
+    @IBOutlet private weak var usernameLabel: UILabel!
+
+    public func setCommentLabel(comment: String){
+        commentLabel.text = comment
+    }
+    
+    public func setUsernameLabel(username: String){
+        usernameLabel.text = username
+    }
+    
+    public func getCommentLabel()->String{
+        return commentLabel!.text ?? CommentCell.COMMENT_ERROR
+    }
+    
+    public func getUsernameLabel()->String{
+        return usernameLabel!.text ?? CommentCell.USERNAME_ERROR
+    }
+}
