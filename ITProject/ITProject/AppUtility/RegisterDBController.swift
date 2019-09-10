d8442d6df3660fc0da0c67ba4fddafbdec21239d
Gilbert Vincenta <gilbert11323@gmail.com>
2019-09-10 15:15:37 +1000

    added user fields

--- ITProject/ITProject/AppUtility/RegisterDBController.swift
+++ ITProject/ITProject/AppUtility/RegisterDBController.swift
@@ -20,6 +20,8 @@ class RegisterDBController {
     public static let USER_DOCUMENT_FIELD_NAME = "name" // user's name
     public static let USER_DOCUMENT_FIELD_FAMILY = "family" // user's family reference
     public static let USER_DOCUMENT_FIELD_POSITION = "position" // user's position in family
+    public static let USER_DOCUMENT_FIELD_GENDER = "gender" // user's gender
+    public static let USER_DOCUMENT_FIELD_PHONE = "phone" // user's phone #
 
 
     /* constant for FAMILIES collections */
