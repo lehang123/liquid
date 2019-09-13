dd793180f58bbc9d24609c35f1019bc86d35c609
Erya Wen <ewenalhyoo@gmail.com>
2019-09-13 13:58:12 +1000

    added upload thumbfile button on add album form (missing dashed line constraint, set thumbnail image)

---
+++ ITProject/ITProject/Utils/UIButton+DashedBorder.swift
@@ -0,0 +1,23 @@
+//
+//  UIButton+DashedBorder.swift
+//  ITProject
+//
+//  Created by Erya Wen on 2019/9/13.
+//  Copyright © 2019 liquid. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+    func createDashedLine(bound: CGRect, color: UIColor, strokeLength: NSNumber, gapLength: NSNumber, width: CGFloat) {
+        let pathlayer = CAShapeLayer()
+        let bounds = bound
+        pathlayer.path = UIBezierPath(roundedRect: bounds, byRoundingCorners: .allCorners, cornerRadii: CGSize(width: 10, height: 10)).cgPath
+        pathlayer.strokeColor = color.cgColor
+        pathlayer.fillColor = UIColor.clear.cgColor
+        pathlayer.lineDashPattern = [strokeLength, gapLength]
+        pathlayer.lineWidth = width
+        layer.addSublayer(pathlayer)
+    }
+}
