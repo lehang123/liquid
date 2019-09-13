70eae49273e5e63a987d84787398b3d30a1d6d58
Erya Wen <ewenalhyoo@gmail.com>
2019-09-13 21:07:24 +1000

    no message

--- ITProject/ITProject/AlbumUI/AddAlbumUI/FormElement.swift
+++ ITProject/ITProject/AlbumUI/AddAlbumUI/FormElement.swift
@@ -1,43 +1,43 @@
+////
+////  FormElement.swift
+////  ITProject
+////
+////  Created by Erya Wen on 2019/9/13.
+////  Copyright © 2019 liquid. All rights reserved.
+////
 //
-//  FormElement.swift
-//  ITProject
+//import Foundation
+//import UIKit
 //
-//  Created by Erya Wen on 2019/9/13.
-//  Copyright © 2019 liquid. All rights reserved.
+//public struct FormElement{
+//    public enum FormType {
+//        case withImageView
+//        case withoutImageView
+//    }
 //
-
-import Foundation
-import UIKit
-
-public struct FormElement{
-    public enum FormType {
-        case withImageView
-        case withoutImageView
-    }
-    
-    /** title Text String */
-    public let titleText: String
-    
-    /** okButton Text String **/
-    public let okButtonText : String
-    /** Image, Title, Description */
-    
-    /** okButton Text String **/
-    public let textFields: [CustomTextFieldContent]
-    
-    /** okButton Text String **/
-    public let cancelButtonText : String
-    
-    public let simpleMessage: EKSimpleMessage
-    
-    /** Contents of button bar */
-    public let buttonBarContent: EKProperty.ButtonBarContent
-    
-    public init(simpleMessage: EKSimpleMessage,
-                imagePosition: ImagePosition = .top,
-                buttonBarContent: EKProperty.ButtonBarContent) {
-        self.simpleMessage = simpleMessage
-        self.imagePosition = imagePosition
-        self.buttonBarContent = buttonBarContent
-    }
-}
+//    /** title Text String */
+//    public let titleText: String
+//
+//    /** okButton Text String **/
+//    public let okButtonText : String
+//    /** Image, Title, Description */
+//
+//    /** okButton Text String **/
+//    public let textFields: [CustomTextFieldContent]
+//
+//    /** okButton Text String **/
+//    public let cancelButtonText : String
+//
+//    public let simpleMessage: EKSimpleMessage
+//
+//    /** Contents of button bar */
+//    public let buttonBarContent: EKProperty.ButtonBarContent
+//
+//    public init(simpleMessage: EKSimpleMessage,
+//                imagePosition: ImagePosition = .top,
+//                buttonBarContent: EKProperty.ButtonBarContent) {
+//        self.simpleMessage = simpleMessage
+//        self.imagePosition = imagePosition
+//        self.buttonBarContent = buttonBarContent
+//    }
+//}
