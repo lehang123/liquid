9ffaf7635ce5e3e25b039cee9693adaa0769a336
Erya Wen <ewenalhyoo@gmail.com>
2019-09-16 15:26:49 +1000

    preview of upload photo in form, TODO: general form content, image setting from database

--- ITProject/ITProject/AlbumUI/AddAlbumUI/FormElement.swift
+++ ITProject/ITProject/AlbumUI/AddAlbumUI/FormElement.swift
@@ -1,43 +1,55 @@
-////
-////  FormElement.swift
-////  ITProject
-////
-////  Created by Erya Wen on 2019/9/13.
-////  Copyright © 2019 liquid. All rights reserved.
-////
 //
-//import Foundation
-//import UIKit
+//  FormElement.swift
+//  ITProject
 //
-//public struct FormElement{
-//    public enum FormType {
-//        case withImageView
-//        case withoutImageView
-//    }
+//  Created by Erya Wen on 2019/9/13.
+//  Copyright © 2019 liquid. All rights reserved.
 //
-//    /** title Text String */
-//    public let titleText: String
-//
-//    /** okButton Text String **/
-//    public let okButtonText : String
-//    /** Image, Title, Description */
-//
-//    /** okButton Text String **/
-//    public let textFields: [CustomTextFieldContent]
-//
-//    /** okButton Text String **/
-//    public let cancelButtonText : String
-//
-//    public let simpleMessage: EKSimpleMessage
-//
-//    /** Contents of button bar */
-//    public let buttonBarContent: EKProperty.ButtonBarContent
-//
-//    public init(simpleMessage: EKSimpleMessage,
-//                imagePosition: ImagePosition = .top,
-//                buttonBarContent: EKProperty.ButtonBarContent) {
-//        self.simpleMessage = simpleMessage
-//        self.imagePosition = imagePosition
-//        self.buttonBarContent = buttonBarContent
-//    }
-//}
+
+import Foundation
+import UIKit
+
+public struct FormElement{
+    public enum FormType {
+        case withImageView
+        case withoutImageView
+    }
+    
+    public typealias Action = () -> ()
+    
+    /** title Text String */
+    public let titleText: String
+
+    /** okButton Text String **/
+    public let textFields: [CustomTextFieldContent]
+    
+    /** okButton Text String **/
+    public let okButtonText: String
+
+    /** okButton Text String **/
+    public let cancelButtonText: String
+    
+    public var okAction: Action?
+    
+    public var cancelAction: Action?
+    
+    public let formType: FormType
+
+
+    public init(titleText: String,
+                textFields: [CustomTextFieldContent],
+                okButtonText: String,
+                okAction: @escaping Action = {},
+                cancelButtonText: String,
+                cancelAction: @escaping Action = {},
+                formType: FormType
+                ){
+        self.titleText = titleText
+        self.textFields = textFields
+        self.okButtonText = okButtonText
+        self.cancelButtonText = cancelButtonText
+        self.okAction = okAction
+        self.cancelAction = cancelAction
+        self.formType = formType
+    }
+}
