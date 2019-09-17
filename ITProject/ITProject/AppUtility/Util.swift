06574c805c0c7d8ff9323876bd668dc922d16017
Erya Wen <ewenalhyoo@gmail.com>
2019-09-17 12:37:58 +1000

    change to defaultImage (water king:)))

--- ITProject/ITProject/AppUtility/Util.swift
+++ ITProject/ITProject/AppUtility/Util.swift
@@ -23,7 +23,7 @@ class Util {
     public static let TMP_FOLDER = "tmp"
     public static let FIREBASE_STORAGE_URL = "gs://liquid-248305.appspot.com/"
     
-    public static let DEFAULT_IMAGE = "default"
+    public static let DEFAULT_IMAGE = "defaultImage"
     
     public static func GenerateUDID () -> String!{
         let uuid = UUID().uuidString
