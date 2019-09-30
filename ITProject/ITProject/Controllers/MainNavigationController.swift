3de7d2341f4abe1383cbdf894803cf5cb8412a74
lehang123 <gonglehan2014@gmail.com>
2019-09-30 23:06:22 +1000

    fixed keyboard bug : todo, in every single viewController, put    self.hideKeyboardWhenTappedAround() in viewDidLoad

--- ITProject/ITProject/Controllers/MainNavigationController.swift
+++ ITProject/ITProject/Controllers/MainNavigationController.swift
@@ -19,6 +19,19 @@ class MainNavigationController: UINavigationController
 	}
 }
 
+// Put this piece of code anywhere you like
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
 extension UIApplication
 {
 	/// Back to the root controller
