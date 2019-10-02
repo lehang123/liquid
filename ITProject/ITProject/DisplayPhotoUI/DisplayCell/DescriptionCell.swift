496b35b4e484e5d6ec8a56ac8b639fdb798d4092
Gilbert Vincenta <gilbert11323@gmail.com>
2019-10-02 14:43:03 +1000

    testing download audio: buggy

--- ITProject/ITProject/DisplayPhotoUI/DisplayCell/DescriptionCell.swift
+++ ITProject/ITProject/DisplayPhotoUI/DisplayCell/DescriptionCell.swift
@@ -8,19 +8,65 @@
 
 import Foundation
 import UIKit
+import AVFoundation
+import AVKit
 
-class DescriptionCell: UITableViewCell {
+class DescriptionCell: UITableViewCell ,AVAudioPlayerDelegate{
     
     private static let COMMENT_ERROR = "Error reading comment"
     private static let USERNAME_ERROR = "Error reading username"
+    private var audioUID : String!
+    private var audioPlayer : AVAudioPlayer!
+
+    
+    public func setAudioUID(audioUID:String){
+
+        self.audioUID = audioUID
+    }
     
 
+    @IBAction func onClickPlayAudio(_ sender: Any) {
+        print("PLEASE WORK ON CLICK")
+       
+        if(audioPlayer?.isPlaying ?? false)
+            {
+                audioPlayer.stop()
+            }
+            else
+            {
+                Util.GetLocalFileURL(by: self.audioUID, type: .audio){
+                   url in
+                    let newURL = URL(fileURLWithPath: url!.absoluteString)
+                    print("URL prepare_play", newURL)
+
+                    self.prepare_play(url: newURL)
+               }
+              
+            }
+           
+        
+        
+    }
+    func prepare_play(url: URL)
+    {
+        do
+        {
+            
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer.delegate = self
+            audioPlayer.prepareToPlay()
+            audioPlayer.play()
+        }
+        catch{
+            print("Error")
+        }
+    }
     @IBOutlet weak var descriptionDetail: UILabel!
     
     @IBOutlet weak var playAudioButton: UIButton!
     public func setDescriptionLabel(description: String){
         if (description.isEmpty) {
-            descriptionDetail.text = "No description yest dlaslkdabksjdlakkladkhasl kdhladkla jdakj ldaksl d ajldkla dladakld jlka ldak ;ld;ad as djkla skd jasd lasdas"
+            descriptionDetail.text = "Click to add description..."
         } else {
             descriptionDetail.text = description
         }
